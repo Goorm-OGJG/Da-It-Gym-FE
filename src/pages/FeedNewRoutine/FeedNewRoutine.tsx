5cfa0ef1676bf6c6580c08b1e49ef61b47f77f9e
Narcoker <jykim9868@gmail.com>
2025-01-08 16:24:14 +0900

    Fix: 루틴 작성시 택스트 비어있어도 리다이렉트되는 버그 해결

--- src/pages/FeedNewRoutine/FeedNewRoutine.tsx
+++ src/pages/FeedNewRoutine/FeedNewRoutine.tsx
@@ -9,7 +9,7 @@ import Button from "../../components/Button/Button";
 import TextArea from "../../components/TextArea/TextArea";
 import * as S from "./FeedNewRoutine.style";
 import { useLocation, useNavigate } from "react-router";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 function FeedNewRoutine() {
   const location = useLocation();
@@ -33,8 +33,6 @@ function FeedNewRoutine() {
       routine: routine,
     };
     requestCreateRoutine(payload);
-    toast.success("루틴을 등록했습니다.");
-    navigate("/feed/routine");
   };
 
   const handleCancel = () => {
